c82db1655bfa933ba32ec60e2f12dda012ba3a85
Vansh Vasishtha <vansh19092002@gmail.com>
2023-03-25 16:27:12 +0530

    Counter

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function increment ()
+{
+   var number = Number(document.getElementById('number').innerHTML)
+   0 
+    document.getElementById('number').innerHTML = number+1
+}
+
+function decrement()
+{
+    var number = Number(document.getElementById('number').innerHTML)
+    0 
+     document.getElementById('number').innerHTML = number-1
+
+}
+
+function neutral()
+{
+
+   
+    0 
+     document.getElementById('number').innerHTML = 0
+
+
+}
